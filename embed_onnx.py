2f13b30974e1672418a8c2441c650279d00439a0
TejasLamba2006 <tejas22feb@gmail.com>
2026-06-10 10:51:45 +0530

    use embed whisper decoder and add embedd decoder

--- embed_onnx.py
+++ embed_onnx.py
@@ -1,12 +1,12 @@
 import onnx
 
 model = onnx.load(
-    "whisper-onnx-exporter/onnx-models/tiny/encoder.onnx",
+    "whisper-onnx-exporter/onnx-models/tiny/decoder.onnx",
     load_external_data=True
 )
 
 onnx.save_model(
     model,
-    "whisper-onnx-exporter/onnx-models/tiny/encoder_embedded.onnx",
+    "whisper-onnx-exporter/onnx-models/tiny/decoder_embedded.onnx",
     save_as_external_data=False
 )
